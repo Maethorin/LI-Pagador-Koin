cabbb92744d7c1d06de1b1a10e01609b964dbbaf
maethorin <maethorin@gmail.com>
2014-07-24 21:21:49 -0300

    Obter configuração de plugin instalado.

--- meio_pagamento/koin/configuracao.py
+++ meio_pagamento/koin/configuracao.py
@@ -3,6 +3,9 @@
 from pagador.configuracao.cadastro import CampoFormulario
 
 
-class PagamentoConfiguracao(object):
+class MeioPagamentoConfiguracao(object):
     consumer_key = CampoFormulario("token", "Consumer Key", requerido=True, tamanho_max=128)
     secret_key = CampoFormulario("senha", "Secret Key", requerido=True, tamanho_max=128)
+
+    def to_dict(self):
+        return {}
