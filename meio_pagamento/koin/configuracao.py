5008f72827b7478a33ffec3d79a5af93fc71c7b7
maethorin <maethorin@gmail.com>
2014-07-25 10:38:45 -0300

    Retornando valores de configuração

--- meio_pagamento/koin/configuracao.py
+++ meio_pagamento/koin/configuracao.py
@@ -12,3 +12,20 @@ class MeioPagamentoFormulario(object):
             "consumer_key": self.consumer_key.to_dict(),
             "secret_key": self.secret_key.to_dict()
         }
+
+
+class MeioPagamentoValores(object):
+    def __init__(self, model):
+        self.model = model
+
+    def to_dict(self):
+        return {
+            'consumer_key': {
+                'campo': 'token',
+                'valor': self.model.token
+            },
+            'secret_key': {
+                'campo': 'senha',
+                'valor': self.model.senha
+            }
+        }
