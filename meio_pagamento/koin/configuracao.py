75522dfadaf4abb41e5e6345f70bd24d791a1033
maethorin <maethorin@gmail.com>
2014-07-24 22:06:04 -0300

    Obter campos de formulário de configuração de plugin instalado.

--- meio_pagamento/koin/configuracao.py
+++ meio_pagamento/koin/configuracao.py
@@ -3,9 +3,12 @@
 from pagador.configuracao.cadastro import CampoFormulario
 
 
-class MeioPagamentoConfiguracao(object):
+class MeioPagamentoFormulario(object):
     consumer_key = CampoFormulario("token", "Consumer Key", requerido=True, tamanho_max=128)
     secret_key = CampoFormulario("senha", "Secret Key", requerido=True, tamanho_max=128)
 
     def to_dict(self):
-        return {}
+        return {
+            "consumer_key": self.consumer_key.to_dict(),
+            "secret_key": self.secret_key.to_dict()
+        }
