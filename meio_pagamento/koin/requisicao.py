9fa3b7b4b89d8bba239b97c1f0e3b720a34775e3
Márcio Duarte <maethorin@gmail.com>
2014-07-28 16:43:04 -0300

    Request para a Koin funcionando

--- meio_pagamento/koin/requisicao.py
+++ meio_pagamento/koin/requisicao.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from meio_pagamento import settings
 from meio_pagamento.koin.pedido import Pedido, Comprador, DocumentoDeComprador, Telefone, Endereco, FormaEnvio, Item
 
 from pagador.envio.requisicao import EnviarPedidoBase
@@ -8,11 +9,16 @@ def formata_data(data):
     return data.strftime("%Y-%m-%d %H:%M:%S")
 
 
+def formata_decimal(valor):
+    return '{0:.3g}'.format(valor)
+
+
 class EnviarPedido(EnviarPedidoBase):
-    def __init__(self, pedido, dados):
-        super(EnviarPedido, self).__init__(pedido, dados)
+    def __init__(self, meio_pagamento, pedido, dados, usa_autenticacao=True):
+        super(EnviarPedido, self).__init__(meio_pagamento, pedido, dados, usa_autenticacao=usa_autenticacao)
         self._comprador_telefones = []
         self.dados = self.gerar_dados_de_envio()
+        self.url = settings.REQUEST_URL
 
     def gerar_dados_de_envio(self):
         pedido_envio = Pedido(
@@ -20,14 +26,14 @@ class EnviarPedido(EnviarPedidoBase):
             reference=self.pedido.numero,
             currency="BRL",
             request_date=formata_data(self.pedido.data_criacao),
-            price=self.pedido.valor_total,
-            discount_value=self.pedido.valor_deconto,
+            price=formata_decimal(self.pedido.valor_total),
+            discount_value=formata_decimal(self.pedido.valor_desconto),
             is_gift=False,
             payment_type=21,
             buyer=Comprador(
                 name=self.pedido.cliente.nome,
                 ip=self.dados["ip"],
-                is_first_purchase=self.pedido.cliente.primeira_vez,
+                is_first_purchase=self.pedido.cliente.eh_primeira_compra_na_loja,
                 is_reliable=self.pedido.cliente.eh_confiavel,
                 buyer_type=self.tipo(),
                 email=self.pedido.cliente.email,
@@ -47,19 +53,19 @@ class EnviarPedido(EnviarPedidoBase):
                 )
             ),
             shipping=FormaEnvio(
-                price=self.pedido.valor_envio,
+                price=formata_decimal(self.pedido.valor_envio),
                 delivery_date=formata_data(self.pedido.provavel_data_entrega),
                 shipping_type=1,
                 address=Endereco(
-                    city=self.pedido.cliente.endereco_entrega.cidade,
-                    state=self.pedido.cliente.endereco_entrega.estado,
-                    country=self.pedido.cliente.endereco_entrega.pais_extenso,
-                    district=self.pedido.cliente.endereco_entrega.bairro,
-                    street=self.pedido.cliente.endereco_entrega.endereco,
-                    number=self.pedido.cliente.endereco_entrega.numero,
-                    complement=self.pedido.cliente.endereco_entrega.complemento,
-                    zip_code=self.pedido.cliente.endereco_entrega.cep,
-                    address_type=self.tipo(self.pedido.cliente.endereco_entrega.tipo)
+                    city=self.pedido.endereco_entrega.cidade,
+                    state=self.pedido.endereco_entrega.estado,
+                    country=self.pedido.endereco_entrega.pais,
+                    district=self.pedido.endereco_entrega.bairro,
+                    street=self.pedido.endereco_entrega.endereco,
+                    number=self.pedido.endereco_entrega.numero,
+                    complement=self.pedido.endereco_entrega.complemento,
+                    zip_code=self.pedido.endereco_entrega.cep,
+                    address_type=self.tipo(self.pedido.endereco_entrega.tipo)
                 )
             ),
             items=self.items,
@@ -103,7 +109,7 @@ class EnviarPedido(EnviarPedidoBase):
                 reference=item.sku,
                 description=item.nome,
                 quantity=item.quantidade,
-                price=item.preco_venda
+                price=formata_decimal(item.preco_venda)
             )
-            for item in self.pedido.itens
+            for item in self.pedido.itens.all()
         ]
