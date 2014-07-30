b05fb5b47de5b5e70b2363064af874d273d05b3a
Márcio Duarte <maethorin@gmail.com>
2014-07-30 16:38:58 -0300

    Alterando estrutura de pastas para usar pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='li-pagador-koin',
-    version='develop',
+    version='0.0.1a',
     url='https://github.com/lojaintegrada/LI-Pagador-Koin',
     license='MIT',
     description='Meio de pagamento usando o Koin (http://www.koin.com.br/)',
@@ -28,7 +28,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Internet",
     ],
-    packages=['pagador_koin'],
+    packages=find_packages('src'),
     install_requires=['distribute'],
     dependency_links=['git+ssh://git@github.com/lojaintegrada/LI-Pagador.git']
 )
