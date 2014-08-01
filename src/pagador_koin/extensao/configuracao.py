bb51a0e40932aef78ec5d38c477290b36573c7d3
Márcio Duarte <maethorin@gmail.com>
2014-08-01 16:11:23 -0300

    Possibilitando ler script de arquivos em disco.

--- src/pagador_koin/extensao/configuracao.py
+++ src/pagador_koin/extensao/configuracao.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import os
 
 from pagador.configuracao.cadastro import CampoFormulario
 from pagador.envio.cliente import Script, TipoScript
@@ -40,33 +41,27 @@ class MeioPagamentoValores(object):
 
 class MeioPagamentoScript(object):
     source_fraud_id = Script(tipo=TipoScript.source, conteudo="//resources.koin.net.br/scripts/koin.min.js")
-    mensagem_aguarde = Script(tipo=TipoScript.html, conteudo=u'<p class="koin-mensagem alert alert-warning">Comunicando com a Koin. Por favor aguarde...</p>')
+
+    def _caminho_do_arquivo(self, arquivo):
+        diretorio = os.path.dirname(os.path.realpath(__file__))
+        return os.path.join(diretorio, "templates", arquivo)
+
+    @property
+    def css(self):
+        return Script(tipo=TipoScript.css, caminho_arquivo=self._caminho_do_arquivo("style.css"))
 
     @property
     def function_enviar(self):
-        script = Script(tipo=TipoScript.javascript, eh_template=True)
-        script.adiciona_linha('{% load filters %}')
-        script.adiciona_linha('$(function() {')
-        script.adiciona_linha('    GetKoinFraudID(function(guid) {')
-        script.adiciona_linha('        var $koinMensagem = $(".koin-mensagem");')
-        script.adiciona_linha('        $koinMensagem.text("Comunicação estabelecida! Enviando seu pedido");')
-        script.adiciona_linha('        $.getJSON("{% url_loja "checkout_pagador" pedido.numero pagamento.id %}?fraud-id=" + guid + "&ip={% get_client_ip %}")')
-        script.adiciona_linha('            .done(function(data) {')
-        script.adiciona_linha('                  if (data.sucesso) {')
-        script.adiciona_linha('                      $koinMensagem.toggleClass("alert-warning alert-success", 600);')
-        script.adiciona_linha('                  }')
-        script.adiciona_linha('                  else {')
-        script.adiciona_linha('                      $koinMensagem.toggleClass("alert-warning alert-danger", 600);')
-        script.adiciona_linha('                  }')
-        script.adiciona_linha('                  console.log(data); $koinMensagem.text(data.mensagem);')
-        script.adiciona_linha('            });')
-        script.adiciona_linha('    });')
-        script.adiciona_linha('});')
-        return script
+        return Script(tipo=TipoScript.javascript, eh_template=True, caminho_arquivo=self._caminho_do_arquivo("javascript.js"))
+
+    @property
+    def mensagens(self):
+        return Script(tipo=TipoScript.html, caminho_arquivo=self._caminho_do_arquivo("mensagens.html"))
 
     def to_dict(self):
         return [
+            self.css.to_dict(),
             self.source_fraud_id.to_dict(),
             self.function_enviar.to_dict(),
-            self.mensagem_aguarde.to_dict()
+            self.mensagens.to_dict()
         ]
---
+++ src/pagador_koin/extensao/templates/javascript.js
@@ -0,0 +1,22 @@
+//{% load filters %}
+$(function() {
+    GetKoinFraudID(function(guid) {
+        var $koinMensagem = $(".koin-mensagem");
+        $.getJSON('{% url_loja "checkout_pagador" pedido.numero pagamento.id %}?fraud-id=' + guid + '&ip={% get_client_ip %}')
+            .done(function(data) {
+                $koinMensagem.find(".msg-warning").hide(600);
+                if (data.sucesso) {
+                    $koinMensagem.toggleClass("alert-message-warning alert-message-success", 600, function() {
+                        $koinMensagem.find(".msg-success").show(400);
+                    });
+                }
+                else {
+                    $koinMensagem.toggleClass("alert-message-warning alert-message-danger", 600, function() {
+                        var $errorMessage = $("#errorMessage");
+                        $errorMessage.text(data.status + ": " + data.content);
+                        $koinMensagem.find(".msg-danger").show(400);
+                    });
+                }
+            });
+    });
+});
