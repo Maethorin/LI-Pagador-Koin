f7d1cba87c9d88fa98e59ec7d0082e7e056f68f6
Márcio Duarte <maethorin@gmail.com>
2014-08-04 18:40:54 -0300

    Removendo console.log

--- src/pagador_koin/extensao/templates/javascript.js
+++ src/pagador_koin/extensao/templates/javascript.js
@@ -1,11 +1,9 @@
 //{% load filters %}
 $(function() {
     GetKoinFraudID(function(guid) {
-        console.log(guid);
         var $koinMensagem = $(".koin-mensagem");
         $.getJSON('{% url_loja "checkout_pagador" pedido.numero pagamento.id %}?fraud-id=' + guid + '&ip={% get_client_ip %}')
             .done(function(data) {
-                console.log(data);
                 $koinMensagem.find(".msg-warning").hide(600);
                 if (data.sucesso) {
                     $koinMensagem.toggleClass("alert-message-warning alert-message-success", 600, function() {
