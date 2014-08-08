aa8c00478284235e5af90ab7d14e87afb842dd98
Márcio Duarte <maethorin@gmail.com>
2014-08-08 12:36:40 -0300

    Versionando

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-koin',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/lojaintegrada/LI-Pagador-Koin',
     license='MIT',
     description='Meio de pagamento usando o Koin (http://www.koin.com.br/)',
