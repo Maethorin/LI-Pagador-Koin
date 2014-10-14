414197a662991d3791447239fe5ca21b127a038f
Márcio Duarte <maethorin@gmail.com>
2014-10-14 14:45:32 -0300

    Resolvendo erro de mensagem da koin

--- src/pagador_koin/extensao/templates/javascript.js
+++ src/pagador_koin/extensao/templates/javascript.js
@@ -14,30 +14,16 @@ $(function() {
         GetKoinFraudID(function (guid) {
             $.getJSON('{% url_loja "checkout_pagador" pedido.numero pagamento.id %}?fraud_id=' + guid + '&ip={% get_client_ip %}')
                 .done(function (data) {
-                    if (jQuery.ui) {
-                        $koinMensagem.find(".msg-warning").hide(600);
-                    }
-                    else {
-                        $koinMensagem.find(".msg-warning").hide();
-                    }
+                    $koinMensagem.find(".msg-warning").hide();
                     if (data.sucesso) {
                         $koinMensagem.toggleClass("alert-message-warning alert-message-success");
                         $koinMensagem.find(".msg-success").show();
                     }
                     else {
-                        if (jQuery.ui) {
-                            $koinMensagem.toggleClass("alert-message-warning alert-message-danger", 600, function () {
-                                var $errorMessage = $("#errorMessage");
-                                $errorMessage.text(data.status + ": " + data.content);
-                                $koinMensagem.find(".msg-danger").show();
-                            });
-                        }
-                        else {
-                            $koinMensagem.toggleClass("alert-message-warning alert-message-danger");
-                            var $errorMessage = $("#errorMessage");
-                            $errorMessage.text(data.status + ": " + data.content);
-                            $koinMensagem.find(".msg-danger").show();
-                        }
+                        $koinMensagem.toggleClass("alert-message-warning alert-message-danger");
+                        var $errorMessage = $("#errorMessage");
+                        $errorMessage.text(data.status + ": " + data.content.string);
+                        $koinMensagem.find(".msg-danger").show();
                     }
                 });
         });
