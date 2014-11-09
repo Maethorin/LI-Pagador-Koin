abd9cd45722240db6f30b42604b4d797c74821f6
maethorin <maethorin@gmail.com>
2014-11-09 16:31:56 -0200

    Implementando antifraude, refatorando mecanismo de entidade serializavel.

--- src/pagador_koin/extensao/envio.py
+++ src/pagador_koin/extensao/envio.py
@@ -1,74 +1,45 @@
 # -*- coding: utf-8 -*-
 
-from pagador.envio.serializacao import EntidadeSerializavel, Atributo
+from pagador.envio.serializacao import EntidadeSerializavel
 
 
 class Pedido(EntidadeSerializavel):
-    _atributos = ["FraudId", "Reference", "Currency", "RequestDate",
-                  "Price", "DiscountPercent", "DiscountValue", "IncreasePercent", "IncreaseValue",
-                  "IsGift", "PaymentType", "Buyer", "Shipping", "Items"]
-    atributos = [
-        Atributo(atributo, eh_serializavel=(atributo == "Buyer" or atributo == "Shipping"), eh_lista=(atributo == "Items"))
-        for atributo in _atributos
-    ]
+    _atributos = ["FraudId", "Reference", "Currency", "RequestDate", "Price", "DiscountPercent", "DiscountValue", "IncreasePercent", "IncreaseValue", "IsGift", "PaymentType"]
+    _atributos_serializaveis = ["Buyer", "Shipping"]
+    _atributos_lista = ["Items"]
 
 
 class Comprador(EntidadeSerializavel):
-    atributos = [
-        Atributo("Name"), Atributo("Ip"), Atributo("IsFirstPurchase"), Atributo("IsReliable"),
-        Atributo("BuyerType"), Atributo("Email"),
-        Atributo("Documents", eh_lista=True),
-        Atributo("Phones", eh_lista=True),
-        Atributo("AdditionalInfo", eh_lista=True),
-        Atributo("Address", eh_serializavel=True)
-    ]
+    _atributos_lista = ["Documents", "Phones", "AdditionalInfo"]
+    _atributos_serializaveis = ["Address"]
+    _atributos = ["Name", "Ip", "IsFirstPurchase", "IsReliable", "BuyerType", "Email"]
 
 
 class DocumentoDeComprador(EntidadeSerializavel):
-    atributos = [Atributo("Key"), Atributo("Value")]
+    _atributos = ["Key", "Value"]
 
 
 class InformacoesDeComprador(EntidadeSerializavel):
-    atributos = [Atributo("Key"), Atributo("Value")]
+    _atributos = ["Key", "Value"]
 
 
 class Telefone(EntidadeSerializavel):
-    atributos = [Atributo("AreaCode"), Atributo("Number"), Atributo("PhoneType")]
+    _atributos = ["AreaCode", "Number", "PhoneType"]
 
 
 class Endereco(EntidadeSerializavel):
-    atributos = [
-        Atributo("City"),
-        Atributo("State"),
-        Atributo("Country"),
-        Atributo("District"),
-        Atributo("Street"),
-        Atributo("Number"),
-        Atributo("Complement"),
-        Atributo("ZipCode"),
-        Atributo("AddressType")
-    ]
+    _atributos = ["City", "State", "Country", "District", "Street", "Number", "Complement", "ZipCode", "AddressType"]
 
 
 class FormaEnvio(EntidadeSerializavel):
-    atributos = [
-        Atributo("Price"),
-        Atributo("DeliveryDate"),
-        Atributo("ShippingType"),
-        Atributo("Address", eh_serializavel=True)
-    ]
+    _atributos_serializaveis = ["Address"]
+    _atributos = ["Price", "DeliveryDate", "ShippingType"]
 
 
 class Item(EntidadeSerializavel):
-    atributos = [
-        Atributo("Reference"),
-        Atributo("Description"),
-        Atributo("Category"),
-        Atributo("Quantity"),
-        Atributo("Price"),
-        Atributo("Attributes", eh_lista=True)
-    ]
+    _atributos_lista = ["Attributes"]
+    _atributos = ["Reference", "Description", "Category", "Quantity", "Price"]
 
 
 class AtributoDeItem(EntidadeSerializavel):
-    atributos = [Atributo("Key"), Atributo("Value")]
+    _atributos = ["Key", "Value"]
