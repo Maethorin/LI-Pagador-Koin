cee569d09b84be199cafd4c7cb8947eb99c5fdd9
Jeferson Farias Calazans <calazans10@gmail.com>
2015-02-03 15:48:38 -0200

    Atualizando nome dos templates

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-koin',
-    version='1.2.3',
+    version='1.2.4',
     url='https://github.com/lojaintegrada/LI-Pagador-Koin',
     license='MIT',
     description='Meio de pagamento usando o Koin (http://www.koin.com.br/)',
