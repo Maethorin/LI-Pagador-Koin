13e8d50fca34f8c6454d0051464d0fe6cd5f2c91
maethorin <maethorin@gmail.com>
2015-02-14 21:54:58 -0200

    Desfazendo commit no branch errado

--- src/pagador_koin/reloaded/cadastro.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from li_common.padroes import cadastro
-
-
-class FormularioKoin(cadastro.Formulario):
-    ativo = cadastro.CampoFormulario('ativo', 'Pagamento ativo?', requerido=True, tipo=cadastro.TipoDeCampo.boleano, ordem=1)
-    consumer_key = cadastro.CampoFormulario("token", "Consumer Key", requerido=True, tamanho_max=128, ordem=2, formato=cadastro.FormatoDeCampo.ascii)
-    secret_key = cadastro.CampoFormulario("senha", "Secret Key", requerido=True, tamanho_max=128, ordem=3, formato=cadastro.FormatoDeCampo.ascii)
