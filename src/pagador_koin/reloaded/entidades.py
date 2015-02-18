a0afd44192c3f0db6361723d63bc6e0fd3634f36
maethorin <maethorin@gmail.com>
2015-02-18 20:59:30 -0200

    Passando codigo_pagamento no init da configuração

--- src/pagador_koin/reloaded/entidades.py
+++ src/pagador_koin/reloaded/entidades.py
@@ -8,7 +8,7 @@ class ConfiguracaoMeioPagamento(entidades.ConfiguracaoMeioPagamento):
     _campos = ['ativo', 'token', 'senha']
     _codigo_gateway = 9
 
-    def __init__(self, loja_id):
-        super(ConfiguracaoMeioPagamento, self).__init__(loja_id)
+    def __init__(self, loja_id, codigo_pagamento=None):
+        super(ConfiguracaoMeioPagamento, self).__init__(loja_id, codigo_pagamento)
         self.preencher_do_gateway(self._codigo_gateway, self._campos)
         self.formulario = cadastro.FormularioKoin()
