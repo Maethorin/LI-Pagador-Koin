37039b2f902f350b73098145de9ba2c359fcb973
maethorin <maethorin@gmail.com>
2015-03-08 23:55:22 -0300

    Versionando

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def read(fname):
 
 setup(
     name='li-pagador-koin',
-    version='1.2.5',
+    version='2.0.0',
     url='https://github.com/lojaintegrada/LI-Pagador-Koin',
     license='MIT',
     description='Meio de pagamento usando o Koin (http://www.koin.com.br/)',
